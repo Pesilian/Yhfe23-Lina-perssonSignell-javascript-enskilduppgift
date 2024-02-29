96eedc9955bcc4588841c4973f74aa962f7939c1
Lina Persson Signell <linasignell@live.se>
2024-02-29 21:52:42 +0100

    small changes

--- overlay_module.js
+++ overlay_module.js
@@ -7,13 +7,22 @@ const renderPlanetInfo = function (array) {
   const html = `
   <div class="modal">
   <button class="close_btn">&times;</button>
-      <h3 class="planet_name">${array.name}</h3>
-      <h4 class="planet_latin">${array.latinName}</h4>
-      <p class="planet_desc">${array.desc}
+  <div class="planet_cont">
+  <section class="planet_header">
+      <h3 class="planet_name infotext">${array.name}</h3>
+      <h4 class="planet_latin infotext">${array.latinName}</h4>
+      <p class="planet_type infotext">Typ: ${array.type}</p>
+  </section>
+  <section class="planet_info">
+      <p class="planet_moons infotext">Moons: ${array.moons}</p>
+      <p class="planet_temp infotext">Dagstemperatur:${array.temp.day}g grader / Natttemperatur:${array.temp.night} grader</p>
+  </section>
+  </div>
+  <section class="planet_description">
+    <p class="planet_desc infotext">${array.desc}
       </p>
-      <p class="planet_type">Typ: ${array.type}</p>
-      <p class="planet_moons">${array.moons}</p>
-      <p class="planet_temp">Dagstemperatur:${array.temp.day}g grader / Natttemperatur:${array.temp.night} grader</p>
+  </section>
+      
    </div>
    <div class="overlay"></div>`;
   infoContainer.insertAdjacentHTML('afterbegin', html);
@@ -24,6 +33,25 @@ const renderPlanetInfo = function (array) {
     //VID KLICK LADDAS SIDAN OM FÖR ATT MÖJLIGGÖRA NY SÖKNING
     location.reload();
   });
+
+  document.querySelector('.arrow_left').addEventListener('click', function () {
+    console.log('hej');
+    // if (planetIndex >= 0) {
+    //   renderPlanetInfo(array - 1);
+    // } else {
+    //   renderPlanetInfo(array + 8);
+    // }
+  });
+
+  document.querySelector('.arrow_right').addEventListener('click', function () {
+    console.log('hå');
+    // if (planetIndex <= 7) {
+    //   renderPlanetInfo(array + 1);
+    // } else {
+    //   renderPlanetInfo(array - 8);
+    // }
+  });
 };
+
 //EXPORT TILL SCRIP.JS
 export { renderPlanetInfo };
